23d629bbbc68c622aac901ce17c782cddc43de5a
fulton <gprlabs@outlook.com>
2019-09-30 19:37:33 -0700

    made block nav system

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/blocknav.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.framework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class blocknav {
+   public int main(int currentBlock){
+       return imageBlockMap.get(currentBlock);
+   }
+   private Map<Integer, Integer> imageBlockMap= new HashMap<Integer, Integer>(){{
+       put(1, 4);
+       put(2,5);
+       put(3,6);
+       put(4,1);
+       put(5,2);
+       put(6,3);
+   }};
+
+}
