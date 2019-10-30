6564b70c0b39831a17f24bbdbe59a486033297ff
fulton <gprlabs@outlook.com>
2019-10-29 18:54:50 -0700

    changed up the move ctrls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Move.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Move.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.framework;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Move {
 
@@ -11,27 +12,30 @@ public class Move {
         motor3.setPower(0.0);
     }
     public void forward(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
-        motor0.setPower(0.0);
-        motor1.setPower(0.0);
-        motor2.setPower(0.0);
-        motor3.setPower(0.0);
+        motor0.setPower(-0.5d);
+        motor1.setPower(0.5d);
+        motor2.setPower(0.5d);
+        motor3.setPower(-0.5d);;
     }
     public void back(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
         motor0.setPower(0.0);
-        motor1.setPower(0.0);
-        motor2.setPower(0.0);
+        motor1.setPower(-0.5);
+        motor2.setPower(-0.5);
         motor3.setPower(0.0);
     }
     public void right(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
-        motor0.setPower(0.0);
-        motor1.setPower(0.0);
-        motor2.setPower(0.0);
-        motor3.setPower(0.0);
+        motor0.setPower(-0.5d);
+        motor1.setPower(-0.5d);
+        motor2.setPower(0.5d);
+        motor3.setPower(0.5d);
     }
     public void left(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
         motor0.setPower(0.0);
         motor1.setPower(0.0);
-        motor2.setPower(0.0);
-        motor3.setPower(0.0);
+        motor2.setPower(-0.5);
+        motor3.setPower(-0.5);
+    }
+    public void setServoPos(Servo servo,Double pos){
+        servo.setPosition(pos);
     }
 }
