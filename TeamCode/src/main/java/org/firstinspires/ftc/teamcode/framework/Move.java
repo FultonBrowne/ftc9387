2341ec9f155a0e1ddb7555b5de9c417e4dcd54f3
fulton <gprlabs@outlook.com>
2019-11-04 18:40:03 -0800

    motor reconfig

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Move.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/Move.java
@@ -2,10 +2,7 @@ package org.firstinspires.ftc.teamcode.framework;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.jetbrains.annotations.Nullable;
 
 public class Move {
 
@@ -16,39 +13,39 @@ public class Move {
         motor3.setPower(0.0);
     }
     public void forward(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
-        //motor0.setPower(-0.5);
-        //motor1.setPower(0.5);
-        //motor2.setPower(-0.5);
-        //motor3.setPower(0.5);
-        motor1.setPower(-1.0);
-        motor3.setPower(1.0);
+        motor0.setPower(-0.5);
+        motor1.setPower(0.5);
+        motor2.setPower(-0.5);
+        motor3.setPower(0.5);
+        //motor1.setPower(-1.0);
+        //motor3.setPower(1.0);
         ;
     }
     public void back(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
-        //motor0.setPower(0.5);
-        //motor1.setPower(-0.5);
-        //motor2.setPower(0.5);
-        //motor3.setPower(-0.5);
-        motor1.setPower(1.0);
-        motor3.setPower(-1.0);
+        motor0.setPower(0.5);
+        motor1.setPower(-0.5);
+        motor2.setPower(0.5);
+        motor3.setPower(-0.5);
+        //motor1.setPower(1.0);
+        //motor3.setPower(-1.0);
     }
     public void right(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
 
-        //motor0.setPower(0.5);
-        //motor1.setPower(-0.5);
-        //motor2.setPower(0.5);
-        //motor3.setPower(-0.5);
-        motor0.setPower(1.0);
-        motor2.setPower(-1.0);
+        motor0.setPower(0.5);
+        motor1.setPower(-0.5);
+        motor2.setPower(0.5);
+        motor3.setPower(-0.5);
+        //motor0.setPower(1.0);
+        //motor2.setPower(-1.0);
     }
     public void left(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3){
 
-        //motor0.setPower(-0.5);
-        //motor1.setPower(0.5);
-        //motor2.setPower(-0.5);
-        //motor3.setPower(0.5);
-        motor0.setPower(-1.0);
-        motor2.setPower(1.0);
+        motor0.setPower(-0.5);
+        motor1.setPower(0.5);
+        motor2.setPower(-0.5);
+        motor3.setPower(0.5);
+        //motor0.setPower(-1.0);
+        //motor2.setPower(1.0);
     }
     public void setServoPos(Servo servo,Double pos){
         servo.setPosition(pos);
