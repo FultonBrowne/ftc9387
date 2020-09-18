7e4668ee1bdb30ec03b53322cc6cc7eb6be34022
fultons gentoo machine <gprlabs@outlook.com>
2020-09-18 14:58:26 -0700

    added some protocode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/AutoFramework.kt
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.framework
+/**
+* Shared code for auto modes
+* GPL 3.0 licensed
+**/
+class AutoFramework(
+   val motor0:DcMotor,
+   val motor1:DcMotor,
+   val motor2:DcMotor,
+   val motor3:DcMotor
+){
+   fun forward(){
+
+   }
+
+   fun left(){
+
+   }
+
+   fun right(){
+
+   }
+
+   fun back(){
+
+   }
+
+   fun stop(){
+
+   }
+
+}
