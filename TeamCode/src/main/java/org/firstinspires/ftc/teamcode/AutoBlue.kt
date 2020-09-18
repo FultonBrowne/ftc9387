e185c116777b97b7dde45766c586ab213a9eb448
fultons gentoo machine <gprlabs@outlook.com>
2020-09-18 15:48:10 -0700

    added some gamepad things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.kt
@@ -5,8 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.hardware.*
 @Autonomous( name = "Auto Blue 2020")
 class AutoBlue : OpMode() {
+   var motor0:DcMotor? = null 
+   var motor1:DcMotor? = null 
+   var motor2:DcMotor? = null 
+   var motor3:DcMotor? = null 
    override fun init(){
-
+        motor0 = hardwareMap.dcMotor["motor0"]
+        motor1 = hardwareMap.dcMotor["motor1"]
+        motor2 = hardwareMap.dcMotor["motor2"]
+        motor3 = hardwareMap.dcMotor["motor3"]
    }
 
 
